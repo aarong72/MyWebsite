d916245eb86419e2bcd7f9e80127faca7ee1798a
Aaron Galonis <73754660+aarong72@users.noreply.github.com>
2022-05-18 12:46:16 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,28 @@
+
+<form method="post" action="subscriberform.php">
+
+
+<textarea name="message"></textarea>
+
+
+<input type="submit">
+
+
+</form>
+
+<php
+
+
+if($_POST["message"]) {
+
+
+mail("your@email.address", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+>
